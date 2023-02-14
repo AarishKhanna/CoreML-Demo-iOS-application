a2eb2b2fefbdd46db9e9034805d8551f3fb61973
Aarish Khanna <aarishkhanna@gmail.com>
2023-02-14 15:53:44 +0530

    Initial Commit

---
+++ exampleProject/exampleProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  exampleProjectApp.swift
+//  exampleProject
+//
+//  Created by Aarish Khanna on 14/02/23.
+//
+
+import SwiftUI
+
+@main
+struct exampleProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
